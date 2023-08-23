f8375d60598732b0062bb1e861c26d6d876f4dd2
Matheus Franco <cajondefox@gmail.com>
2023-08-23 06:48:27 -0300

    feat: add Timeline component

--- apps/article/src/app/app.module.ts
+++ apps/article/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, TimelineComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
