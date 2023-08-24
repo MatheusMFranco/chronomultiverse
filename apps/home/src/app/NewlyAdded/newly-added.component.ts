081f2c8d8c7e950a942bef053ecbbd868b2c8062
Matheus Franco <cajondefox@gmail.com>
2023-08-24 17:25:33 -0300

    feat: add Newly Added component

---
+++ apps/home/src/app/NewlyAdded/newly-added.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chronomultiverses-newly-added',
+  templateUrl: './newly-added.component.html',
+  styleUrls: ['./newly-added.component.css'],
+})
+export class NewlyAddedComponent {}
--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NewlyAddedComponent } from './newly-added/newly-added.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, NewlyAddedComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
