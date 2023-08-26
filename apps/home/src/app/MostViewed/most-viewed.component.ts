df9d7760cea6126c8f0af08087a8e8af08d20002
Matheus Franco <cajondefox@gmail.com>
2023-08-25 21:41:29 -0300

    feat: add Most Viewed component

---
+++ apps/home/src/app/MostViewed/most-viewed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chronomultiverses-most-viewed',
+  templateUrl: './most-viewed.component.html',
+  styleUrls: ['./most-viewed.component.css'],
+})
+export class MostViewedComponent {}
--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { NewlyAddedComponent } from './newly-added/newly-added.component';
+import { MostViewedComponent } from './most-viewed/most-viewed.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, NewlyAddedComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    NewlyAddedComponent,
+    MostViewedComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
