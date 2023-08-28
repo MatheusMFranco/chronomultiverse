e1e8adc4b9e44f72b192a779cc5b52b6815e0705
Matheus Franco <cajondefox@gmail.com>
2023-08-28 20:29:35 -0300

    feat: add Last Updated component

---
+++ apps/home/src/app/LastUpdated/last-updated.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chronomultiverses-last-updated',
+  templateUrl: './last-updated.component.html',
+  styleUrls: ['./last-updated.component.css'],
+})
+export class LastUpdatedComponent {}
--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { NewlyAddedComponent } from './newly-added/newly-added.component';
 import { MostViewedComponent } from './most-viewed/most-viewed.component';
+import { LastUpdatedComponent } from './last-updated/last-updated.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { MostViewedComponent } from './most-viewed/most-viewed.component';
     NxWelcomeComponent,
     NewlyAddedComponent,
     MostViewedComponent,
+    LastUpdatedComponent,
   ],
   imports: [BrowserModule],
   providers: [],
