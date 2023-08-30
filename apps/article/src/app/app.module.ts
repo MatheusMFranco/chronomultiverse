fecd31e20e4ab10f4d55803a8cd596a5b3a725d3
Matheus Franco <cajondefox@gmail.com>
2023-08-30 15:22:01 -0300

    refact: remove auto generated framework content

--- apps/article/src/app/app.module.ts
+++ apps/article/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, TimelineComponent],
+  declarations: [AppComponent, TimelineComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
