3a6ff808d9ca6c2e60be37782655765621e2ef4c
Matheus Franco <cajondefox@gmail.com>
2023-09-02 12:36:49 -0300

    fix: timeline import

--- apps/article/src/app/app.module.ts
+++ apps/article/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TimelineComponent } from './timeline/timeline.component';
+import { TimelineComponent } from './Timeline/timeline.component';
 
 @NgModule({
   declarations: [AppComponent, TimelineComponent],
