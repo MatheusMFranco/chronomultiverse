409721c67b62f56ba5e0934216e8a09e02279249
Matheus Franco <cajondefox@gmail.com>
2023-10-20 00:00:33 -0300

    feat: update timeline model to add pieces

--- apps/article/src/app/core/models/timeline.model.ts
+++ apps/article/src/app/core/models/timeline.model.ts
@@ -1,12 +1,13 @@
+import { Era } from '../enums/era.enum';
+import { Piece } from './piece.model';
+
 export interface Timeline {
   name: string;
-  cover?: string;
   description: string;
-  franchise: string;
-  serie?: string;
-  universe?: string;
   isCanon: boolean;
   isRemake: boolean;
   isReboot: boolean;
   isOriginal: boolean;
+  era: Era;
+  pieces: Piece;
 }
