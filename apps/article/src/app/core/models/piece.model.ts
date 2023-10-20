6357fa50dc8c89f0dc14119a46a2ffd10461e113
Matheus Franco <cajondefox@gmail.com>
2023-10-19 23:59:36 -0300

    feat: update piece model

--- apps/article/src/app/core/models/piece.model.ts
+++ apps/article/src/app/core/models/piece.model.ts
@@ -1,13 +1,13 @@
+import { Format } from '../enums/format.enum';
+
 export interface Piece {
   name: string;
   description: string;
-  cover?: string;
-  franchise: string;
-  serie?: string;
-  timeline?: string;
-  universe?: string;
+  cover: string;
+  serie: string;
   isCanon: boolean;
   isRemake: boolean;
   isReboot: boolean;
   isOriginal: boolean;
+  format: Format;
 }
