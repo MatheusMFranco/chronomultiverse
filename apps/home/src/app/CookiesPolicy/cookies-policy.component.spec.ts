42be0ea912b5e06a1c1efbcfd8f3d5310e091d57
Matheus Franco <cajondefox@gmail.com>
2023-11-28 16:14:00 -0300

    refact: change CookiesPolicy test name

--- apps/home/src/app/CookiesPolicy/cookies-policy.component.spec.ts
+++ apps/home/src/app/CookiesPolicy/cookies-policy.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CookiesPolicyComponent } from './cookies-policy.component';
 
-describe('CookiesPolicyComponent', () => {
+describe(CookiesPolicyComponent.name, () => {
   let component: CookiesPolicyComponent;
   let fixture: ComponentFixture<CookiesPolicyComponent>;
 
