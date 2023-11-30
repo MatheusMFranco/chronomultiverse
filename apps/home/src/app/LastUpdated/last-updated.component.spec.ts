ae00ed450a20183e5ababb365ef5eafeb560dee3
Matheus Franco <cajondefox@gmail.com>
2023-11-29 23:57:27 -0300

    refact: last updated component test name

--- apps/home/src/app/LastUpdated/last-updated.component.spec.ts
+++ apps/home/src/app/LastUpdated/last-updated.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LastUpdatedComponent } from './last-updated.component';
 
-describe('LastUpdatedComponent', () => {
+describe(LastUpdatedComponent.name, () => {
   let component: LastUpdatedComponent;
   let fixture: ComponentFixture<LastUpdatedComponent>;
 
